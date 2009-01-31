3c26811b1cac2c9fdb605375e9a5895ea464ded1
eko <eko@edc2f09a-efd1-11dd-ac5b-dfceb64ead24>
2009-01-31 01:55:54 +0000

    Tag v0.1.5!

---
+++ tags/Halo3-v0.1.5/src/net/kylelemons/halo3/GameGenerator.java
@@ -0,0 +1,336 @@
+/**
+ * @file GameGenerator.java by Kyle Lemons, created Jan 17, 2009
+ */
+package net.kylelemons.halo3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+/**
+ * @author eko
+ *
+ */
+public class GameGenerator implements ListDataListener
+{
+  public static interface GameChangedListener
+  {
+    public void gameChanged(GameGenerator gen);
+  }
+  
+  private static Logger logger = Logger.getLogger("net.kylelemons.halo3");
+  
+  /** Keep track of the player list */
+  private PlayerList m_playerlist;
+  
+  /** Keep track of the game list */
+  private GameList m_gamelist;
+  
+  /** Keep track of the map list */
+  private MapList m_maplist;
+  
+  /** Keep track of listeners */
+  private ArrayList<GameChangedListener> m_listeners;
+  
+  /** Keep track of the setup */
+  private GameSetup m_setup;
+  
+  /** Save the generated teams */
+  private Team[] m_teams;
+  
+  /** Save the generated map */
+  private MapList.Map m_map;
+  
+  /** Save the generated game */
+  private GameList.GameType m_game;
+
+  private long m_seed;
+  private Random m_rand;
+  private boolean m_good;
+  
+  public GameGenerator(GameSetup setup)
+  {
+    m_playerlist = new PlayerList();
+    m_gamelist = new GameList();
+    m_maplist = new MapList();
+    m_setup = setup;
+    m_listeners = new ArrayList<GameChangedListener>();
+    m_rand = new Random();
+    m_seed = -1;
+    m_good = true;
+  }
+  
+  public GameGenerator()
+  {
+    this(new GameSetup());
+  }
+  
+  public void setPlayerList(PlayerList list)
+  {
+    m_playerlist = list;
+    m_playerlist.addListDataListener(this);
+  }
+  
+  public void setGameList(GameList list)
+  {
+    m_gamelist = list;
+    m_gamelist.addListDataListener(this);
+  }
+  
+  public void setMapList(MapList list)
+  {
+    m_maplist = list;
+    m_maplist.addListDataListener(this);
+  }
+
+  public void contentsChanged(ListDataEvent e)
+  {
+    regenerate();
+  }
+
+  public void intervalAdded(ListDataEvent e)
+  {
+    regenerate();
+  }
+
+  public void intervalRemoved(ListDataEvent e)
+  {
+    regenerate();
+  }
+  
+  public synchronized void regenerate()
+  {
+    // Don't do anything until the game has been explicitly generated
+    if (m_seed == -1)
+      return;
+    
+    // Regenerate the game
+    if (m_seed == 0)
+    {
+      m_seed = System.currentTimeMillis();
+      logger.info("Generating game...");
+    }
+    else
+      logger.info("Regenerating game...");
+    
+    // Reset the PRNG
+    m_rand.setSeed(m_seed);
+    
+    // Wipe the team list clean
+    m_teams = null;
+    /*
+    m_teams = new Team[m_setup.getTeamCount()];
+    for (int t = 0; t < m_teams.length; ++t)
+    {
+      m_teams[t] = new Team("Team " + (t+1));
+    }
+    */
+    
+    // Collect all of the players
+    ArrayList<PlayerList.Player> players = new ArrayList<PlayerList.Player>();
+    for (int i = 0; i < m_playerlist.getSize(); ++i)
+    {
+      PlayerList.Player p = m_playerlist.getPlayers(false).get(i); // don't autosort! This calls this method again!
+      if (p.playing)
+        players.add(p);
+    }
+    
+    /* * * * * * * * * * * * * * * * */
+    /* Start Player allocation code  */
+    /* * * * * * * * * * * * * * * * */
+    
+    // Order list by: Priority, Skill, Random
+    //Collections.shuffle(players, m_rand);
+    //Collections.sort(players, new PlayerList.Player.SkillSorter());    // guaranteed to be stable, so it preserves
+    //Collections.sort(players, new PlayerList.Player.PrioritySorter()); // prior order when equal
+    
+    int totalPlayers = players.size();
+    int realPlayers = (totalPlayers <= 16)?totalPlayers:16;
+    int maxPerTeam = (int)Math.ceil( (double)totalPlayers / m_setup.getTeamCount() );
+    int fairness = m_setup.getFairness();
+    if (totalPlayers < 2)
+    {
+      logger.info("Cowardly refusing to create a game with fewer than two people playing");
+      return;
+    }
+    
+    logger.fine("With " + players.size() + " players, we'll have at most " + maxPerTeam + " players per team");
+    
+    // Loop fairness times to find the fairest team allocation
+    int fairest_score = -1;
+    for (int i = 0; i <= fairness; ++i)
+    {
+      int fairness_score;
+      
+      // Generate the teams
+      Collections.shuffle(players, m_rand);
+      
+      // Allocate a clean list of teams
+      Team[] potential = new Team[m_setup.getTeamCount()];
+      
+      // Assign people to teams
+      int cur = 0;
+      for (int t = 0; t < potential.length; ++t)
+      {
+        int cap = m_setup.getTeamCap(t);
+        if (cap == 0 || cap > maxPerTeam) cap = maxPerTeam;
+        potential[t] = new Team("Team " + (t+1));
+        for (int j = 0; j < cap && cur < players.size(); ++j)
+          potential[t].add(players.get(cur++));
+      }
+      // This is good if the teams come out evenly.  Otherwise, the caps are messed up.
+      m_good = cur == players.size();
+      
+      fairness_score = getFairTeamScore(potential);
+      
+      if (m_teams == null || fairness_score < fairest_score)
+      {
+        m_teams = potential;
+        fairest_score = fairness_score;
+        logger.info("New fairest team: " + fairest_score);
+      }
+    }
+    
+    int[] mapWeights = new int[m_maplist.getSize()];
+    for (int m = 0; m < mapWeights.length; ++m)
+    {
+      if (m == 0) mapWeights[m] = 0;
+      else        mapWeights[m] = mapWeights[m-1];
+      
+      MapList.Map map = m_maplist.getMapList(false).get(m);
+      
+      if (map.enabled && map.min_players <= realPlayers && realPlayers <= map.max_players)
+        mapWeights[m] += map.weight;
+    }
+    
+    if (m_maplist.getSize() < 1 || mapWeights[mapWeights.length-1] <= 0)
+    {
+      logger.info("Cowardly refusing to create a game with fewer than one valid map");
+      return;
+    }    
+    
+    int chosenMap = m_rand.nextInt(mapWeights[mapWeights.length-1]);
+    for (int m = 0; m < mapWeights.length; ++m)
+      if (chosenMap < mapWeights[m])
+      {
+        m_map = m_maplist.getMapList(false).get(m);
+        break;
+      }
+
+    int[] gameWeights = new int[m_gamelist.getSize()];
+    for (int g = 0; g < gameWeights.length; ++g)
+    {
+      if (g == 0) gameWeights[g] = 0;
+      else        gameWeights[g] = gameWeights[g-1];
+      
+      GameList.GameType game = m_gamelist.getGameList(false).get(g);
+      
+      if (game.enabled)
+        gameWeights[g] += game.weight;
+    }
+    
+    if (m_gamelist.getSize() < 1 || gameWeights[gameWeights.length-1] <= 0)
+    {
+      logger.info("Cowardly refusing to create a game with fewer than one valid game");
+      return;
+    }
+    
+    int chosenGame = m_rand.nextInt(gameWeights[gameWeights.length-1]);
+    for (int g = 0; g < gameWeights.length; ++g)
+      if (chosenGame < gameWeights[g])
+      {
+        m_game = m_gamelist.getGameList(false).get(g);
+        break;
+      }
+    
+    fireGameChanged();
+  }
+  
+  private int getFairTeamScore(Team[] potential)
+  {
+    int max;
+    int min;
+    
+    if (potential.length <= 0) return -1;
+    
+    max = min = potential[0].totalSkill();
+    
+    for (int t = 1; t < potential.length; ++t)
+    {
+      if (t == potential.length-1 && m_setup.getIgnoreLastTeam())
+        continue;
+      int skill = potential[t].totalSkill();
+      if (skill > max)
+        max = skill;
+      if (skill < min)
+        min = skill;
+    }
+    
+    return max-min;
+  }
+
+  public void generate()
+  {
+    m_seed = 0;
+    regenerate();
+  }
+  
+  public void addGameChangedListener(GameChangedListener listener)
+  {
+    m_listeners.add(listener);
+  }
+  
+  private void fireGameChanged()
+  {
+    ArrayList<Integer> deleteIndices = new ArrayList<Integer>();
+    for (int i = 0; i < m_listeners.size(); ++i)
+    {
+      try
+      {
+        m_listeners.get(i).gameChanged(this);
+      }
+      catch (Exception e)
+      {
+        e.printStackTrace();
+        logger.severe("Exception in listener! Deleting.");
+        deleteIndices.add(i);
+      }
+    }
+    // Remove from last to first to maintain indices
+    for (int d = deleteIndices.size()-1; d >= 0; ++d)
+    {
+      m_listeners.remove(deleteIndices.get(d));
+    }
+  }
+  
+  public Team[] getTeams()
+  {
+    return m_teams;
+  }
+
+  public GameList.GameType getGame()
+  {
+    return m_game;
+  }
+
+  public MapList.Map getMap()
+  {
+    return m_map;
+  }
+
+  public void setSetup(GameSetup setup)
+  {
+    m_setup = setup;
+    regenerate();
+  }
+
+  public boolean getGood()
+  {
+    // TODO Auto-generated method stub
+    return m_good;
+  }
+}
---
+++ tags/Halo3-v0.1.5/src/net/kylelemons/halo3/HaloGameMaster.java
@@ -0,0 +1,278 @@
+package net.kylelemons.halo3;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import net.kylelemons.halo3.GameList.GameType;
+import net.kylelemons.halo3.GameSetup.SetupChangeListener;
+import net.kylelemons.halo3.MapList.Map;
+import net.kylelemons.halo3.PlayerList.Player;
+import net.kylelemons.halo3.RemoteUpdateClient.RemoteUpdateListener;
+
+public class HaloGameMaster implements GameGenerator.GameChangedListener, UserInterface.KeyListener, SetupChangeListener, RemoteUpdateListener
+{
+  private UserInterface      m_ui;
+  private GameGenerator      m_gen;
+  private PlayerList         m_players;
+  private GameList           m_games;
+  private MapList            m_maps;
+
+  private GameSetup          m_setup;
+
+  // TODO Logging
+  public static final String APPLICATION = "HaloGameMaster";
+  public static final String VERSION     = "v0.1.5";
+  private long               m_regen_start_time;
+  private SimpleWebInterface m_webserver;
+  private Thread             m_webthread;
+  private RemoteUpdateServer m_remoteupdate;
+  private Thread             m_remotethread;
+  private Thread             m_clientthread;
+  private RemoteUpdateClient m_remoteclient;
+  private boolean            m_clientstarted;
+  private boolean            m_serverstarted;
+
+  private static Logger      logger      = Logger.getLogger("net.kylelemons.halo3");
+
+  /**
+   * @param args
+   * @throws InterruptedException
+   * @throws IOException
+   * @throws SecurityException
+   */
+  public static void main(String[] args) throws InterruptedException, SecurityException, IOException
+  {
+
+    HaloGameMaster master = new HaloGameMaster();
+    master.start();
+  }
+
+  public HaloGameMaster() throws SecurityException, IOException
+  {
+    // Setup logging
+    // Send logging output to file
+    FileHandler fh = new FileHandler("Halo3.log"); // "%t/Halo3.log"
+    fh.setFormatter(new SimpleFormatter());
+    logger.addHandler(fh); // fh.setFormatter(new SimpleFormatter());
+
+    // Request that every detail gets logged.
+    logger.setLevel(Level.ALL);
+
+    // Log a simple INFO message.
+    logger.info(APPLICATION + "-" + VERSION + " Starting...");
+
+    logger.fine("Creating Generator Setup");
+    m_setup = new GameSetup();
+
+    logger.fine("Creating Game Generator");
+    m_gen = new GameGenerator(m_setup);
+
+    logger.fine("Creating User Interface");
+    m_ui = new UserInterface();
+    m_ui.setSetup(m_setup);
+
+    logger.fine("Creating Default Configuration");
+    // These are the default players if none are loaded from the database
+    m_players = new PlayerList();
+    m_players.add(new PlayerList.Player("Example", "ex", 2));
+    m_players.add(new PlayerList.Player("Players", "Zap", 3));
+    m_players.add(new PlayerList.Player("Add Your Own", "Eko", 5));
+    m_players.add(new PlayerList.Player("Press F1", "F1", 1, 50, false));
+    m_players.add(new PlayerList.Player("Press F2", "F2", 2));
+    m_players.add(new PlayerList.Player("Press F3", "F3", 3));
+    m_players.add(new PlayerList.Player("Press F4", "F4", 4));
+    m_players.add(new PlayerList.Player("Hold F5", "F5", 5));
+    m_players.add(new PlayerList.Player("Quit is \"q\"", "quit", 0));
+    m_players.sort(PlayerList.Player.SORT_BY_NAME);
+    m_ui.setPlayerList(m_players);
+    m_gen.setPlayerList(m_players);
+
+    m_games = new GameList();
+    m_games.add(new GameList.GameType("Team Slayer", 5));
+    m_games.add(new GameList.GameType("Team CTF", 2, false));
+    m_games.add(new GameList.GameType("Team King", 2));
+    m_games.add(new GameList.GameType("VIP", 1));
+    m_games.add(new GameList.GameType("Zombies", 1, false));
+    m_games.add(new GameList.GameType("Rocket Swords", 1, false));
+    m_games.add(new GameList.GameType("Shotty Sniper", 1));
+    m_games.add(new GameList.GameType("Ninja", 1, false));
+    m_games.sort();
+    m_ui.setGameList(m_games);
+    m_gen.setGameList(m_games);
+
+    m_maps = new MapList();
+    m_maps.add(new MapList.Map("Blackout", "images/Blackout.jpg", 4, 10, 3));
+    m_maps.add(new MapList.Map("Cold Storage", "images/Cold_Storage.jpg", 2, 6, 3));
+    m_maps.add(new MapList.Map("Foundry", "images/Foundry.jpg", 4, 10, 3));
+    m_maps.add(new MapList.Map("Ghost Town", "images/Ghost_Town.jpg", 4, 12, 3));
+    m_maps.add(new MapList.Map("Guardian", "images/Guardian.jpg", 2, 8, 3));
+    m_maps.add(new MapList.Map("Epitaph", "images/Epitaph.jpg", 4, 10, 2));
+    m_maps.add(new MapList.Map("Narrows", "images/Narrows.jpg", 4, 10, 2));
+    m_maps.add(new MapList.Map("Avalanche", "images/Avalanche.jpg", 10, 16, 1));
+    m_maps.add(new MapList.Map("Construct", "images/Construct.jpg", 8, 12, 1));
+    m_maps.add(new MapList.Map("High Ground", "images/High_Ground.jpg", 6, 12, 1));
+    m_maps.add(new MapList.Map("Isolation", "images/Isolation.jpg", 8, 10, 1));
+    m_maps.add(new MapList.Map("Last Resort", "images/Last_Resort.jpg", 4, 16, 1));
+    m_maps.add(new MapList.Map("Rat's Nest", "images/Rat's_Nest.jpg", 6, 16, 1));
+    m_maps.add(new MapList.Map("Sandtrap", "images/Sandtrap.jpg", 10, 16, 1));
+    m_maps.add(new MapList.Map("Snowbound", "images/Snowbound.jpg", 12, 16, 1));
+    m_maps.add(new MapList.Map("Valhalla", "images/Valhalla.jpg", 10, 16, 1));
+    m_maps.sort();
+    m_ui.setMapList(m_maps);
+    m_gen.setMapList(m_maps);
+
+    logger.fine("Running UI Setup");
+    m_ui.setup(); // the last thing this does is load the database, overwriting
+    // the above
+
+    m_setup.addSetupChangeListener(this);
+
+    logger.info("Setting up web server");
+    m_webserver = new SimpleWebInterface();
+
+    logger.info("Setting up remote update server");
+    m_remoteupdate = new RemoteUpdateServer();
+    m_serverstarted = false;
+
+    logger.info("Setting up remote update client");
+    m_remoteclient = new RemoteUpdateClient();
+    m_clientstarted = false;
+
+    logger.info("Setup Complete");
+  }
+
+  public void start()
+  {
+    logger.info("Starting " + APPLICATION);
+    m_gen.addGameChangedListener(this);
+    m_ui.addKeyListener(this);
+    m_gen.generate();
+
+    logger.info("Starting webserver");
+    m_webthread = new Thread(m_webserver);
+    m_webthread.start();
+
+    m_remotethread = new Thread(m_remoteupdate);
+
+    m_clientthread = new Thread(m_remoteclient);
+  }
+
+  public void gameChanged(GameGenerator gen)
+  {
+    Team[] teams = gen.getTeams();
+    GameType game = gen.getGame();
+    Map map = gen.getMap();
+    boolean good = gen.getGood();
+
+    if (!m_clientstarted)
+    {
+      logger.info("Updating game UI");
+      m_ui.setGame(teams, game, map);
+      m_webserver.setGame(teams, game, map);
+      m_remoteupdate.setGame(teams, game, map, m_setup.getGameDelay());
+      m_ui.setWarningBorder(good ? Color.BLACK : Color.RED);
+    }
+    else
+      logger.info("Ignoring local update -- in client mode");
+  }
+
+  public void MainScreenKeyHit(int keyCode, int eventID)
+  {
+    switch (keyCode)
+    {
+      case KeyEvent.VK_R:
+      case KeyEvent.VK_F5:
+      {
+        if (m_clientstarted)
+        {
+          // We're a remote client -- don't take local updates!
+          m_ui.setWarningBorder(Color.YELLOW);
+          break;
+        }
+        if (eventID == KeyEvent.KEY_PRESSED && m_regen_start_time == 0)
+          m_regen_start_time = System.currentTimeMillis();
+        else
+        {
+          long held = System.currentTimeMillis() - m_regen_start_time;
+          if (held > 4000)
+          {
+            logger.info("Game Generation requested from keyboard");
+            m_gen.generate();
+            m_regen_start_time = System.currentTimeMillis();
+          }
+          if (eventID == KeyEvent.KEY_RELEASED) m_regen_start_time = 0;
+        }
+        break;
+      }
+      case KeyEvent.VK_C:
+      {
+        if (!m_clientstarted)
+        {
+          logger.info("Starting remote update client");
+          m_remoteclient.setIP(m_setup.getServerHost());
+          // m_remoteclient.removeRemoteUpdateListener(this);
+          m_remoteclient.addRemoteUpdateListener(this);
+          m_clientthread.start();
+          m_clientstarted = true;
+        }
+        m_ui.setWarningBorder(Color.GREEN);
+        break;
+      }
+      case KeyEvent.VK_H:
+      {
+        if (!m_serverstarted)
+        {
+          logger.info("Starting remote update server");
+          m_remotethread.start();
+          m_serverstarted = true;
+        }
+        m_ui.setWarningBorder(Color.BLUE);
+        break;
+      }
+    }
+  }
+
+  public void setupChanged()
+  {
+    m_gen.generate();
+  }
+
+  public void RemoteError(String error, Throwable exception)
+  {
+    Team[] teams = new Team[1];
+    GameType game = new GameType("Error");
+    Map map = new Map("Remote Update");
+
+    teams[0] = new Team("Error");
+    teams[0].add(new Player(error, "Error"));
+
+    logger.info("Updating game UI (remote error)");
+    m_ui.setGame(teams, game, map);
+    m_webserver.setGame(teams, game, map);
+    m_ui.setWarningBorder(Color.RED);
+
+    m_clientstarted = false;
+    m_clientthread = new Thread(m_remoteclient);
+  }
+
+  public void RemoteUpdate(Team[] teams, GameType game, Map map, int delay)
+  {
+    if (!m_clientstarted)
+    {
+      logger.warning("Ignoring remote update -- in local mode!");
+      return;
+    }
+    logger.info("Updating game UI (remote)");
+    m_setup.setGameDelay(delay);
+    m_ui.setGame(teams, game, map);
+    m_webserver.setGame(teams, game, map);
+    m_ui.setWarningBorder(Color.BLACK);
+    m_remoteupdate.setGame(teams, game, map, delay); // allows proxying
+  }
+
+}
